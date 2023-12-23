f0463885100527b6e934aa878379571b4e69df3d
十三 <zonghailiu@creditease.cn>
2023-12-24 07:21:59 +0800

    事务消息

--- spring-rabbitmq/src/main/java/com/spring/rabbitmq/producer/TransactionProducer.java
+++ spring-rabbitmq/src/main/java/com/spring/rabbitmq/producer/TransactionProducer.java
@@ -1,6 +1,7 @@
 package com.spring.rabbitmq.producer;
 
 import jakarta.annotation.Resource;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,13 @@ public class TransactionProducer {
 
     @Transactional(rollbackFor = Exception.class, transactionManager = "rabbitTransactionManager")
     public void sendMessage(String message) {
-        rabbitTemplate.convertAndSend("test.exchange", "test.routing", message);
+        //捕获异常不抛出，消息会发送成功的。
+        try{
+            CorrelationData correlationData = new CorrelationData(message);
+            rabbitTemplate.convertAndSend("test.exchange", "test.routing", message, correlationData);
+            throw new RuntimeException("异常了");
+        }catch (Exception e){
+
+        }
     }
 }
