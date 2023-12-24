62b1215ca1a7effea0ac2fec12c7e0eb86f02118
十三 <zonghailiu@creditease.cn>
2023-12-24 23:08:09 +0800

    注册和配置。

---
+++ spring-flow-control/src/main/java/com/spring/flowcontrol/FlowControlApplication.java
@@ -0,0 +1,22 @@
+package com.spring.flowcontrol;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @auth 十三先生
+ * @date 2023/12/24
+ * @desc
+ */
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+
+public class FlowControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowControlApplication.class);
+    }
+}
--- spring-rabbitmq/src/main/java/com/spring/rabbitmq/RabbitMQApplication.java
+++ spring-rabbitmq/src/main/java/com/spring/rabbitmq/RabbitMQApplication.java
@@ -1,12 +1,10 @@
 package com.spring.rabbitmq;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@EnableAdminServer
 @SpringBootApplication
 public class RabbitMQApplication {
 
