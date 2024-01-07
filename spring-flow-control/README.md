80914b4c24b10e91f531e03d2fade2f64ee048d9
十三 <zonghailiu@creditease.cn>
2024-01-07 23:22:46 +0800

    缓存 Redisson限流

--- spring-flow-control/README.md
+++ spring-flow-control/README.md
@@ -1,9 +1,10 @@
-
 **环境搭建**
-    
+
     spring-boot 版本3.0.2
     jdk版本 17 
     数据库：MySQL Redis
     配置中心：Nacos
     注册中心：Nacos
     Spring-boot-admin
+
+    sentinel 单机限流配置
--- spring-flow-control/src/main/java/com/spring/flowcontrol/FlowControlApplication.java
+++ spring-flow-control/src/main/java/com/spring/flowcontrol/FlowControlApplication.java
@@ -13,7 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
-
 public class FlowControlApplication {
 
     public static void main(String[] args) {
