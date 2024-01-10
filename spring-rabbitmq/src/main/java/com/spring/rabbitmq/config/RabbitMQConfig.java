77c24dcf3b17ab94183fc6da452cba2d5568f8b4
十三 <zonghailiu@creditease.cn>
2024-01-10 23:25:49 +0800

    持久化队列和队列声明字段说明

--- spring-rabbitmq/src/main/java/com/spring/rabbitmq/config/RabbitMQConfig.java
+++ spring-rabbitmq/src/main/java/com/spring/rabbitmq/config/RabbitMQConfig.java
@@ -62,6 +62,11 @@ public class RabbitMQConfig {
 
     @Bean("directExchange")
     public Exchange directExchange() {
+        // exclusive：排他队列，如果一个队列被声明为排他队列，该队列仅对首次申明它的连接可见，并在连接断开时自动删除
+        // 1. 排他队列是基于连接可见的，同一连接的不同信道是可以同时访问同一连接创建的排他队列；
+        // 2.“首次”，如果一个连接已经声明了一个排他队列，其他连接是不允许建立同名的排他队列的，这个与普通队列不同；
+        // 3.即使该队列是持久化的，一旦连接关闭或者客户端退出，该排他队列都会被自动删除的，这种队列适用于一个客户端发送读取消息的应用场景
+        // autoDelete：自动删除，如果该队列没有任何订阅的消费者的话，该队列会被自动删除。这种队列适用于临时队列。
         Exchange exchange = new DirectExchange(Constants.EXCHANGE, Boolean.TRUE, Boolean.FALSE);
         return exchange;
     }
